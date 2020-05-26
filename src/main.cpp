727f0211bca7b6cc565ccdb8d5cff98e64997c93
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2020-05-26 21:35:40 +0300

    Add example with simple bluetooth communication to turn on a LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,47 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
-#include "Arduino.h"
+#include <arduino.h>
+#include <softwareserial.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN 13
+#define BLUETOOTH_RX 10
+#define BLUETOOTH_TX 11
+
+SoftwareSerial BT(BLUETOOTH_RX, BLUETOOTH_TX);
+// creates a "virtual" serial port/UART
+// connect BT module RX to D10
+// connect BT module TX to D11
+// connect BT Vcc to 5V, GND to GND
 
 void setup()
 {
-  // initialize LED digital pin as an output.
+  // set digital pin to control as an output
   pinMode(LED_BUILTIN, OUTPUT);
+  // set the data rate for the SoftwareSerial port
+  BT.begin(9600);
+  // Send test message to other device
+  BT.println("Hello from Arduino");
 }
-
+char received_chr; // stores incoming character from other device
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  // wait for a second
-  delay(2000);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-
-   // wait for a second
-  delay(500);
+  if (BT.available())
+  // if text arrived in from BT serial...
+  {
+    received_chr = BT.read();
+    if (received_chr == '1')
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      BT.println("LED on");
+    }
+    if (received_chr == '2')
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+      BT.println("LED off");
+    }
+    if (received_chr == '?')
+    {
+      BT.println("Send '1' to turn LED on");
+      BT.println("Send '2' to turn LED off");
+    }
+    // you can add more "if" statements with other characters to add more commands
+  }
 }
