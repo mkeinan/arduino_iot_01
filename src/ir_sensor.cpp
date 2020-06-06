7b1b23741b559fac85024bcd4649cf093ff17d86
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2020-06-06 16:34:55 +0300

    First attempt to configure the vehicle's movement control flow

---
+++ src/ir_sensor.cpp
@@ -0,0 +1,36 @@
+/*
+
+// This program prints the value of the input from the TCRT5000 IR sensor.
+// An example of serial communication.
+
+#include <arduino.h>
+#include <softwareserial.h>
+
+// analog pin of potentiometer:
+const int IR_SENSOR = A1;
+const int IR_THRESHOLD = 150;  // Totally ampirical value => might have to change
+bool black_line_detected = false;
+
+void setup() {
+  pinMode(IR_SENSOR, INPUT);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the potentiometer:
+  int val = analogRead(IR_SENSOR);
+
+  black_line_detected = val > 150;
+  if (black_line_detected) {
+    Serial.println("Black line / I see nothing");
+  } else {
+    Serial.println("Floor / IR reflecting object detected");
+  }
+
+  // print out the value that was read:
+  Serial.println(val);
+
+  delay(200);  // delay in between reads
+}
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,169 @@
 
-/*
-This program prints the value of the input from the TCRT5000 IR sensor.
-An example of serial communication.
-*/
+
+// A Trial to control the vehicle using Bluetooth.
+
+
+
 #include <arduino.h>
 #include <softwareserial.h>
 
-// analog pin of potentiometer:
-const int IR_SENSOR = A1;
-const int IR_THRESHOLD = 150;  // Totally ampirical value => might have to change
-bool black_line_detected = false;
+// Possible vehicle control commands:
+#define STOP 's'
+#define MOVE_FORWARD 'w'
+#define MOVE_BACKWARD 'x'
+#define TURN_LEFT 'a'
+#define TURN_RIGHT 'd'
 
-void setup() {
-  pinMode(IR_SENSOR, INPUT);
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+// Bluetooth pins
+#define LED_BUILTIN 13
+#define BLUETOOTH_RX 12
+#define BLUETOOTH_TX 11
+
+// Bluetooth software object
+SoftwareSerial BT(BLUETOOTH_RX, BLUETOOTH_TX);
+// BT input char:
+char received_chr; // stores incoming character from other device
+
+// car pins:
+int Enable_A = 5;
+int In_1 = 7;
+int In_2 = 8;
+int In_3 = 2;
+int In_4 = 4;
+int Enable_B = 3;
+
+// car speed:
+int speed = 153;  // just an initial value between 0 and 255
+
+
+// some vehicle utility functions:
+void vehicle_stop(){
+  digitalWrite(In_1, HIGH);
+  digitalWrite(In_2, LOW);
+  digitalWrite(In_3, HIGH);
+  digitalWrite(In_4, LOW);
+  analogWrite(Enable_A, 0);
+  analogWrite(Enable_B, 0);
 }
 
-void loop() {
-  // read the potentiometer:
-  int val = analogRead(IR_SENSOR);
+void vehicle_move_forward(){
+  digitalWrite(In_1, HIGH);
+  digitalWrite(In_2, LOW);
+  digitalWrite(In_3, HIGH);
+  digitalWrite(In_4, LOW);
+  analogWrite(Enable_A, speed);
+  analogWrite(Enable_B, speed);
+}
 
-  black_line_detected = val > 150;
-  if (black_line_detected) {
-    Serial.println("Black line / I see nothing");
-  } else {
-    Serial.println("Floor / IR reflecting object detected");
+void vehicle_move_backward(){
+  digitalWrite(In_1, LOW);
+  digitalWrite(In_2, HIGH);
+  digitalWrite(In_3, LOW);
+  digitalWrite(In_4, HIGH);
+  analogWrite(Enable_A, speed);
+  analogWrite(Enable_B, speed);
+}
+
+void vehicle_turn_right(){
+  digitalWrite(In_1, HIGH);
+  digitalWrite(In_2, LOW);
+  digitalWrite(In_3, LOW);
+  digitalWrite(In_4, HIGH);
+  analogWrite(Enable_A, speed);
+  analogWrite(Enable_B, speed);
+}
+
+void vehicle_turn_left(){
+  digitalWrite(In_1, LOW);
+  digitalWrite(In_2, HIGH);
+  digitalWrite(In_3, HIGH);
+  digitalWrite(In_4, LOW);
+  analogWrite(Enable_A, speed);
+  analogWrite(Enable_B, speed);
+}
+
+void vehicle_change_speed(int new_speed){
+  speed = new_speed;
+  analogWrite(Enable_A, speed);
+  analogWrite(Enable_B, speed);
+}
+
+
+// Arduino Setup
+void setup()
+{
+  // initialize all pins on the L298N H-Bridge as outputs:
+  pinMode(Enable_A, OUTPUT);
+  pinMode(In_1, OUTPUT);
+  pinMode(In_2, OUTPUT);
+  pinMode(In_3, OUTPUT);
+  pinMode(In_4, OUTPUT);
+  pinMode(Enable_B, OUTPUT);
+
+  // initialize the vehicle in "STOP" mode:
+  vehicle_stop();
+
+  // init the bluetooth module
+  BT.begin(9600);
+  // Send test message to other device
+  BT.println("Hello from Arduino");
+}
+
+
+
+void loop()
+{
+  if (BT.available())
+  // if text arrived in from BT serial...
+  {
+    received_chr = BT.read();
+    if (received_chr == STOP) {
+      vehicle_stop();
+      BT.println("Stop");
+    }
+    else if (received_chr == MOVE_FORWARD) {
+      vehicle_move_forward();
+      BT.println("Forward");
+    }
+    else if (received_chr == MOVE_BACKWARD) {
+      vehicle_move_backward();
+      BT.println("Reverse");
+    }
+    else if (received_chr == TURN_RIGHT) {
+      vehicle_turn_right();
+      BT.println("Turn Right");
+    }
+    else if (received_chr == TURN_LEFT) {
+      vehicle_turn_left();
+      BT.println("Turn Left");
+    }
+    else if (received_chr == '0') {
+      vehicle_change_speed(0);
+      BT.println("Changing speed");
+    }
+    else if (received_chr == '1') {
+      vehicle_change_speed(51);
+      BT.println("Changing speed");
+    }
+    else if (received_chr == '2') {
+      vehicle_change_speed(102);
+      BT.println("Changing speed");
+    }
+    else if (received_chr == '3') {
+      vehicle_change_speed(153);
+      BT.println("Changing speed");
+    }
+    else if (received_chr == '4') {
+      vehicle_change_speed(204);
+      BT.println("Changing speed");
+    }
+    else if (received_chr == '5') {
+      vehicle_change_speed(255);
+      BT.println("Changing speed");
+    }
+    else {
+      BT.println("Error: unrecognized command... stopping the vehicle");
+      vehicle_stop();
+    }
   }
-
-  // print out the value that was read:
-  Serial.println(val);
-
-  delay(200);  // delay in between reads
 }
