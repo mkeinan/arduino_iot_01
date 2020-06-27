04a9dc89dd667080c07febf40fb6ea0c5ba03916
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2020-06-27 12:35:55 +0300

    Working code for color sensor which detects RGB color format

--- src/color_sensor.cpp
+++ src/color_sensor.cpp
@@ -5,12 +5,24 @@
 #define S1 A2
 #define S2 A3
 #define S3 A5
-#define colorSensor 0
+#define colorSensor 9
+
+#define RED_HIGH_FREQ 100
+#define RED_LOW_FREQ 21
+#define GREEN_HIGH_FREQ 90
+#define GREEN_LOW_FREQ 30
+#define BLUE_HIGH_FREQ 90
+#define BLUE_LOW_FREQ 30
 
 // Stores frequency read by the photodiodes
 int redFrequency = 0;
 int greenFrequency = 0;
 int blueFrequency = 0;
+
+// Stores the red. green and blue colors
+int redColor = 0;
+int greenColor = 0;
+int blueColor = 0;
 
 void setup() {
   // Setting the outputs
@@ -36,10 +48,12 @@ void loop() {
 
   // Reading the output frequency
   redFrequency = pulseIn(colorSensor, LOW);
+  // Remaping the value of the RED (R) frequency from 0 to 255
+  redColor = map(redFrequency, RED_LOW_FREQ, RED_HIGH_FREQ, 255,0);
 
    // Printing the RED (R) value
   Serial.print("R = ");
-  Serial.print(redFrequency);
+  Serial.print(redColor);
   delay(100);
 
   // Setting GREEN (G) filtered photodiodes to be read
@@ -48,10 +62,12 @@ void loop() {
 
   // Reading the output frequency
   greenFrequency = pulseIn(colorSensor, LOW);
+  // Remaping the value of the GREEN (G) frequency from 0 to 255
+  greenColor = map(greenFrequency, GREEN_LOW_FREQ, GREEN_HIGH_FREQ, 255, 0);
 
   // Printing the GREEN (G) value
   Serial.print(" G = ");
-  Serial.print(greenFrequency);
+  Serial.print(greenColor);
   delay(100);
 
   // Setting BLUE (B) filtered photodiodes to be read
@@ -60,9 +76,23 @@ void loop() {
 
   // Reading the output frequency
   blueFrequency = pulseIn(colorSensor, LOW);
+  // Remaping the value of the BLUE (B) frequency from 0 to 255
+  blueColor = map(blueFrequency, BLUE_LOW_FREQ, BLUE_HIGH_FREQ, 255, 0);
 
   // Printing the BLUE (B) value
   Serial.print(" B = ");
-  Serial.println(blueFrequency);
+  Serial.println(blueColor);
   delay(100);
+
+  // Checks the current detected color and prints
+  // a message in the serial monitor
+  if(redColor > greenColor && redColor > blueColor){
+      Serial.println(" - RED detected!");
+  }
+  if(greenColor > redColor && greenColor > blueColor){
+    Serial.println(" - GREEN detected!");
+  }
+  if(blueColor > redColor && blueColor > greenColor){
+    Serial.println(" - BLUE detected!");
+  }
 }
